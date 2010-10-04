3084dd4d411963aa362cd03c57f19c98cde7265b
Andrew Roth <andrewroth@gmail.com>
2010-10-04 16:26:46 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ccc360::Application.config.secret_token = 'e07245aa8bf585036eca88e1ae6151d09d58217eab3a7d11a79db911a1299a59d23391ac9933d20020226c086fc898e4788f503fd34bfd86bdc1f94371aabd14'
