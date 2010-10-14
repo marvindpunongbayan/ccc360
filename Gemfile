5d86ca4c9e56a00668254d43f2b57a5d4ec04b5a
Andrew Roth <andrewroth@gmail.com>
2010-10-14 13:25:02 -0400

    questionnaire engine migrations done

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.0'
 gem 'mysql', '2.8.1'
+gem 'ruby-debug'
+gem 'acts_as_list'
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,114 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20101012150519) do
+
+  create_table "answer_sheet_question_sheets", :force => true do |t|
+    t.integer  "answer_sheet_id"
+    t.integer  "question_sheet_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pr_answer_sheets", :force => true do |t|
+    t.integer  "question_sheet_id", :null => false
+    t.datetime "created_at",        :null => false
+    t.datetime "completed_at"
+  end
+
+  create_table "pr_answers", :force => true do |t|
+    t.integer "answer_sheet_id", :null => false
+    t.integer "question_id",     :null => false
+    t.text    "value"
+    t.string  "short_value"
+    t.integer "size"
+    t.string  "content_type"
+    t.string  "filename"
+    t.integer "height"
+    t.integer "width"
+    t.integer "parent_id"
+    t.string  "thumbnail"
+  end
+
+  add_index "pr_answers", ["answer_sheet_id"], :name => "index_pr_answers_on_answer_sheet_id"
+  add_index "pr_answers", ["question_id"], :name => "index_pr_answers_on_question_id"
+  add_index "pr_answers", ["short_value"], :name => "index_pr_answers_on_short_value"
+
+  create_table "pr_conditions", :force => true do |t|
+    t.integer "question_sheet_id", :null => false
+    t.integer "trigger_id",        :null => false
+    t.string  "expression",        :null => false
+    t.integer "toggle_page_id",    :null => false
+    t.integer "toggle_id"
+  end
+
+  create_table "pr_elements", :force => true do |t|
+    t.string   "kind",                      :limit => 40, :null => false
+    t.string   "style",                     :limit => 40
+    t.string   "label"
+    t.text     "content"
+    t.boolean  "required"
+    t.string   "slug",                      :limit => 36
+    t.integer  "position"
+    t.string   "object_name"
+    t.string   "attribute_name"
+    t.string   "source"
+    t.string   "value_xpath"
+    t.string   "text_xpath"
+    t.integer  "question_grid_id"
+    t.string   "cols"
+    t.boolean  "is_confidential"
+    t.string   "total_cols"
+    t.string   "css_id"
+    t.string   "css_class"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "related_question_sheet_id"
+    t.integer  "conditional_id"
+  end
+
+  add_index "pr_elements", ["position"], :name => "index_pr_elements_on_question_sheet_id_and_position_and_page_id"
+  add_index "pr_elements", ["slug"], :name => "index_pr_elements_on_slug"
+
+  create_table "pr_page_elements", :force => true do |t|
+    t.integer  "page_id"
+    t.integer  "element_id"
+    t.integer  "position"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pr_pages", :force => true do |t|
+    t.integer "question_sheet_id",                                   :null => false
+    t.string  "label",             :limit => 100,                    :null => false
+    t.integer "number"
+    t.boolean "no_cache",                         :default => false
+    t.boolean "hidden",                           :default => false
+  end
+
+  add_index "pr_pages", ["question_sheet_id", "number"], :name => "page_number", :unique => true
+
+  create_table "pr_question_sheets", :force => true do |t|
+    t.string  "label",    :limit => 60,                    :null => false
+    t.boolean "archived",               :default => false
+  end
+
+  create_table "pr_references", :force => true do |t|
+    t.integer  "question_id"
+    t.integer  "answer_sheet_id"
+    t.integer  "response_id"
+    t.datetime "email_sent_at"
+    t.string   "relationship"
+    t.string   "title"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "status"
+    t.datetime "submitted_at"
+    t.string   "access_key"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
