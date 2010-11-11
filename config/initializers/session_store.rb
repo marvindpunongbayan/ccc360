cc4c20f5f74fe725b250c3311462985ccbb70567
Andrew Roth <andrewroth@gmail.com>
2010-11-11 01:52:56 -0500

    no sessionstore with memcache locally

--- config/initializers/session_store.rb
+++
@@ -1,2 +0,0 @@
-require 'action_dispatch/middleware/session/dalli_store'
-Ccc360::Application.config.session_store :dalli_store, :memcache_server => ['127.0.0.1'], :namespace => 'sessions', :key => '_sp2_session', :expire_after => 2.days
