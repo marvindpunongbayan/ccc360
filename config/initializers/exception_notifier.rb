c80c1556d4b1c702383a92091294157e2be0fe19
Andrew Roth <andrewroth@gmail.com>
2010-12-07 14:53:14 -0500

    much easier to define the server in exception mailer; add Josh

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,35 +1,4 @@
 Ccc360::Application.config.middleware.use ExceptionNotifier,
   :email_prefix => "[PR] ",
   :sender_address => %{"PR Exception Notifier" <notifier@pr.uscm.org>},
-  :exception_recipients => %w{andrewroth@gmail.com}
-
-class ExceptionInterceptor
-  @@smtp_settings_before = ActionMailer::Base.smtp_settings
-  @@live_smtp_settings = {
-    :address   => "smtp1.ccci.org",
-    :domain   => "ccci.org"
-  }
- 
-  def self.delivering_email(mail)
-    if mail.subject =~ /^\[PR\]/
-      live_settings
-    end
-  end
-
-  def self.delivered_email(mail)
-    revert_settings
-  end
-
-  def self.live_settings
-    ActionMailer::Base.smtp_settings = @@live_smtp_settings
-    Rails.logger.info "before - SET smtp settings to: #{ActionMailer::Base.smtp_settings.inspect}"
-  end
-
-  def self.revert_settings
-    ActionMailer::Base.smtp_settings = @@smtp_settings_before
-    Rails.logger.info "after - SET smtp settings to: #{ActionMailer::Base.smtp_settings.inspect}"
-  end
-end
-
-#ActionMailer::Base.register_interceptor(ExceptionInterceptor)
-#ActionMailer::Base.register_observer(ExceptionInterceptor)
+  :exception_recipients => %w{andrewroth@gmail.com josh.starcher@gmail.com}
