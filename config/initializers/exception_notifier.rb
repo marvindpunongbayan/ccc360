7d7bd85c9b7a41c791e97376b1a3d46c6715cf6d
Andrew Roth <andrewroth@gmail.com>
2010-12-07 14:25:52 -0500

    add interceptor/observe to make exception_notification emails live

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,26 +1,26 @@
 Ccc360::Application.config.middleware.use ExceptionNotifier,
-  :email_prefix => "[PR Exception] ",
+  :email_prefix => "[PR] ",
   :sender_address => %{"PR Exception Notifier" <notifier@pr.uscm.org>},
   :exception_recipients => %w{andrewroth@gmail.com}
 
 class ExceptionInterceptor
+  @@live_smtp_settings = {
+    :address   => "smtp1.ccci.org",
+    :domain   => "ccci.org"
+  }
+ 
   def self.delivering_email(mail)
     @@smtp_settings_before = ActionMailer::Base.smtp_settings
-    ActionMailer::Base.smtp_settings = {
-      :address => 'localhost2',
-      :port => 2525,
-      :domain => 'ccci.org'
-    }
-    STDOUT.puts "before - SET smtp settings to: #{ActionMailer::Base.smtp_settings.inspect}"
+    ActionMailer::Base.smtp_settings = @@live_smtp_settings
+    Rails.logger.info "before - SET smtp settings to: #{ActionMailer::Base.smtp_settings.inspect}"
   end
   def self.delivered_email(mail)
     revert_settings
-    #STDOUT.puts "after - SET smtp settings to: #{ActionMailer::Base.smtp_settings.inspect}"
   end
 
   def self.revert_settings
     ActionMailer::Base.smtp_settings = @@smtp_settings_before
-    STDOUT.puts "after - SET smtp settings to: #{ActionMailer::Base.smtp_settings.inspect}"
+    Rails.logger.info "after - SET smtp settings to: #{ActionMailer::Base.smtp_settings.inspect}"
   end
 end
 
