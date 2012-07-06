24f7ec205a4c17701a98f7f544407520df077b3a
Weston Platter <westonplatter@gmail.com>
2012-07-06 11:39:03 -0400

    moved https within ( )

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -47,23 +47,23 @@ class ReviewsController < AnswerSheetsController
 
   def new_submit_form
     session[:new_review][:question_sheet_id] = params[:question_sheet_id]
-    redirect_to new_subject_reviews_path(:format => "js"), :protocol => "https://"
+    redirect_to new_subject_reviews_path(:format => "js", :protocol => "https://")
   end
 
   def new_submit_subject
     session[:new_review][:subject_id] = params[:subject_id]
-    redirect_to new_initiator_reviews_path(:format => "js"), :protocol => "https://"
+    redirect_to new_initiator_reviews_path(:format => "js", :protocol => "https://")
   end
 
   def new_submit_initiator
     session[:new_review] ||= {}
     session[:new_review][:initiator_id] = params[:initiator_id]
-    redirect_to new_details_reviews_path(:format => "js"), :protocol => "https://"
+    redirect_to new_details_reviews_path(:format => "js", :protocol => "https://")
   end
 
   def show
     unless request.xhr?
-      redirect_to collate_review_path(params[:id]), :protocol => "https://"
+      redirect_to collate_review_path(params[:id], :protocol => "https://")
     else
       @review = Review.find params[:id]
     end
